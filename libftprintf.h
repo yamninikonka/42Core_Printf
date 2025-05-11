bd32fe08cdae5dbbb65bd63752ffcef21bde4a98
Yamini Konka <ykonka@2-B-2.42heilbronn.de>
2025-05-11 10:24:22 +0200

    ignore file updated

---
+++ libftprintf.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libftprintf.h                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ykonka <ykonka@student.42.fr>              #+#  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025-05-10 16:10:26 by ykonka            #+#    #+#             */
+/*   Updated: 2025-05-10 16:10:26 by ykonka           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFTPRINTF_H
+# define LIBFTPRINTF_H
+
+# include <unistd.h>
+#include "libft/libft.h"
+
+char	*decimal_to_hexadecimal(int integer, char letter_size);
+
+#endif
